beba92823fae9f504ad39890b04ccea82cbafdd6
galarot <mmd0012@alu.medac.es>
2026-04-15 14:13:11 +0200

    permisos varios

--- disciplinary/ryc_student_disciplinary/models/hr_employee.py
+++ disciplinary/ryc_student_disciplinary/models/hr_employee.py
@@ -5,5 +5,5 @@ class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
     is_tutor = fields.Boolean(string='Es tutor')
-    # curso del que es tutor, solo visible si is_tutor es verdadero
-    course_tutor = fields.Many2one('ryc.course', string='Curso tutor')
+    # grupo del que es tutor, solo visible si is_tutor es verdadero
+    course_tutor = fields.Many2one('ryc.course.group', string='Grupo tutor')
